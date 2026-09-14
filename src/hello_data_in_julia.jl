98e803c8a5d121c04dc02dcd1c2b417bdb764144
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-14 16:09:23 -0600

    Update hello_data_in_julia.jl

--- src/hello_data_in_julia.jl
+++ src/hello_data_in_julia.jl
@@ -1,7 +1,7 @@
 module hello_data_in_julia
 module DatabaseSetup
-    include("database_setup/sqlite.jl")
     include("database_setup/schemas.jl")
+    include("database_setup/sqlite.jl")
 end # DatabaseSetup
 export DatabaseSetup
 
