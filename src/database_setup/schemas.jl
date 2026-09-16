5e4a0ba14143bce0d8e5286a33f7f9c6626ab9d5
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-16 11:32:53 -0600

    Update schemas.jl

--- src/database_setup/schemas.jl
+++ src/database_setup/schemas.jl
@@ -2,30 +2,40 @@ module Schemas
 using Tables
 
 
-const USERS_01 = Tables.Schema(
+const TABLE_01 = Tables.Schema(
     (:user_id, :action, :action_date),
     (Int64, Union{String,Missing}, Union{String,Missing}),
 )
+const TABLE_02 = Tables.Schema(
+    (:sender, :receiver, :amount, :transaction_date),
+    (:Int64, Union{Int64, Missing}, Union{Float64, Missing}, Union{String, Missing}),
+)
+const TABLE_03 = Tables.Schema(
+    (:date, :item),
+    (String, Union{String, Missing}),
+)
+const TABLE_04 = Tables.Schema(
+    (:id, :action, :action_date),
+    (Int64, Union(String, Missing), Union(String, Missing)),
+)
+const TABLE_05 = Tables.Schema(
+    (:user_id, :product_id, :transaction_date),
+    (Int64, Union{Int64, Missing}, Union{String, Missing}),
+)
 
 
-const SCHEMAS = Dict("users_01" => USERS_01)
+const SCHEMAS = Dict(
+    "users_01" => TABLE_01,
+    "transactions_02" => TABLE_02,
+    "items_03" => TABLE_03,
+    "users_04" => TABLE_04,
+    "users_05" => TABLE_05,
+)
 
 
 function csv_types(nombre_tabla::String)
     schema = SCHEMAS[nombre_tabla]
     return Dict(zip(schema.names, schema.types))
-end
-
-
-function csv_path(csv::String)::Union{Vector{String}, String}
-    dir = joinpath(@__DIR__, "..", "..", "data", "csv", csv)
-    if isdir(dir)
-        return joinpath.(dir, filter(f -> endswith(f, ".csv"), readdir(dir)))
-    elseif isfile(dir)
-        return dir
-    else
-        throw(ArgumentError("$csv is invalid"))
-    end
 end
 
 end # module Schemas
