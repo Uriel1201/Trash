fdf9135079568a46dabd81ba132214cb10d4e634
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-17 18:59:53 +0000

    Add data processing modules

--- src/database_setup/schemas.jl
+++ src/database_setup/schemas.jl
@@ -1,41 +1,67 @@
 module Schemas
-using Tables
+
+using TOML
+using Dates
+
+export SCHEMAS, csv_types, load_schemas!
+
+const SCHEMAS = Dict{String, Dict{Symbol, Type}}()
 
 
-const TABLE_01 = Tables.Schema(
-    (:user_id, :action, :action_date),
-    (Int64, Union{String,Missing}, Union{String,Missing}),
-)
-const TABLE_02 = Tables.Schema(
-    (:sender, :receiver, :amount, :transaction_date),
-    (Int64, Union{Int64, Missing}, Union{Float64, Missing}, Union{String, Missing}),
-)
-const TABLE_03 = Tables.Schema(
-    (:date, :item),
-    (String, Union{String, Missing}),
-)
-const TABLE_04 = Tables.Schema(
-    (:id, :action, :action_date),
-    (Int64, Union{String, Missing}, Union{String, Missing}),
-)
-const TABLE_05 = Tables.Schema(
-    (:user_id, :product_id, :transaction_date),
-    (Int64, Union{Int64, Missing}, Union{String, Missing}),
-)
+"""
+    parse_type_string(type_str::String)
+"""
+function parse_type_string(type_str::String)::Type
+    try
+        return Core.eval(@__MODULE__, Meta.parse(type_str))
+    catch e
+        error("Failed to parse type '$type_str' defined in TOML. Error: $e")
+    end
+end
 
 
-const SCHEMAS = Dict(
-    "users_01" => TABLE_01,
-    "transactions_02" => TABLE_02,
-    "items_03" => TABLE_03,
-    "users_04" => TABLE_04,
-    "users_05" => TABLE_05,
-)
+"""
+    load_schemas!(toml_path::String)
+"""
+function load_schemas!(toml_path::String)
+    if !isfile(toml_path)
+        error("$toml_path: does not exist")
+    end
+
+    raw_data = TOML.parsefile(toml_path)
+    empty!(SCHEMAS)
+
+    for (table, columns) in raw_data
+        table_dict = Dict{Symbol, Type}()
+        for (col_name, type_str) in columns
+            table_dict[Symbol(col_name)] = parse_type_string(type_str)
+        end
+        SCHEMAS[table] = table_dict
+    end
+
+    return SCHEMAS
+end
 
 
-function csv_types(nombre_tabla::String)
-    schema = SCHEMAS[nombre_tabla]
-    return Dict(zip(schema.names, schema.types))
+"""
+    csv_types(table::String)
+"""
+function csv_types(table::String)::Dict{Symbol, Type}
+    if isempty(SCHEMAS)
+        error("SCHEMAS dont loaded. Call 'Schemas.load_schemas!(path)' first.")
+    end
+
+    return get(SCHEMAS, table) do
+        error("'$table' isn't registered in the current TOML.")
+    end
+end
+
+
+function __init__()
+    default_path = joinpath(@__DIR__, "..", "..", "config", "schemas.toml")
+    if isfile(default_path)
+        load_schemas!(default_path)
+    end
 end
 
 end # module Schemas
