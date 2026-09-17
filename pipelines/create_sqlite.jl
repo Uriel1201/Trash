5c35c5742be22a81479a7c556d6a56a47469d1c5
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-17 00:15:13 +0000

    Add data processing modules

--- pipelines/create_sqlite.jl
+++ pipelines/create_sqlite.jl
@@ -1,4 +1,5 @@
 using Tables, SQLite
+import hello_data_in_julia.DatabaseSetup.Schemas as sch
 import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 
 
@@ -6,91 +7,23 @@ const TABLE_LIST = dbs.get_conn("hello_data", "ro") do conn
     dbs.my_tables(conn)
 end
 
-#=
-function create_01_users(conn::SQLite.DB)::dbs.MyTable
-    t = "users_01"
-    schema = Tables.Schema(
-        (:user_id, :action, :action_date),
-        (Int64, Union{String,Nothing}, Union{String,Nothing}),
-    )
-    if !(t in TABLE_LIST)
-        return dbs.create_table(conn, t, schema)
+
+function users_01(conn::SQLite.DB)::Nothing
+    table = "users_01"
+    if !(table in dbs.my_tables(conn))
+        SQLite.createtable!(conn, table, sch.SCHEMAS[table], temp = false)
+        @info $table created:$schema.names, $schema.types
     else
-        @info "$t already exists"
-        return dbs.mytable(t, schema)
+        @info "$table already exists"
     end
-end
-
-
-function create_02_transactions(conn::SQLite.DB)::dbs.MyTable
-    t = "02_transactions"
-    schema = Tables.Schema(
-        (:sender, :receiver, :amount, :transaction_date),
-        (Int64, Union{Int64,Nothing}, Union{Float64,Nothing}, Union{String,Nothing}),
-    )
-    if !(t in TABLE_LIST)
-        return dbs.create_table(conn, t, schema)
-    else
-        @info "$t already exists"
-        return dbs.mytable(t, schema)
-    end
-end
-
-
-function create_03_items(conn::SQLite.DB)::dbs.MyTable
-    t = "03_items"
-    schema = Tables.Schema((:date, :item), (String, Union{String,Nothing}))
-    if !(t in TABLE_LIST)
-        return dbs.create_table(conn, t, schema)
-    else
-        @info "$t already exists"
-        return dbs.mytable(t, schema)
-    end
-end
-
-
-function create_04_users(conn::SQLite.DB)::dbs.MyTable
-    t = "04_users"
-    schema = Tables.Schema(
-        (:id, :action, :action_date),
-        (Int64, Union{String,Nothing}, Union{String,Nothing}),
-    )
-    if !(t in TABLE_LIST)
-        return dbs.create_table(conn, t, schema)
-    else
-        @info "$t already exists"
-        return dbs.mytable(t, schema)
-    end
-end
-
-
-function create_05_users(conn::SQLite.DB)::dbs.MyTable
-    t = "05_users"
-    schema =
-        Tables.Schema((:user_id, :product_id, :transaction_date), (Int64, Int64, String))
-    if !(t in TABLE_LIST)
-        return dbs.create_table(conn, t, schema)
-    else
-        @info "$t already exists"
-        return dbs.mytable(t, schema)
-    end
+    nothing
 end
 
 
 function main()
     dbs.get_conn("hello_data", "rw") do conn
         println("uri: $conn connected")
-
-        tables = dbs.MyTable[]
-        push!(tables, create_01_users(conn))
-        push!(tables, create_02_transactions(conn))
-        push!(tables, create_03_items(conn))
-        push!(tables, create_04_users(conn))
-        push!(tables, create_05_users(conn))
-
-        for table in tables
-            println("table:$(table.name), columns: $(table.stmt_columns)")
-        end
+        users_01(conn)
     end
 end
 
@@ -98,4 +31,3 @@ end
 if Base.@isdefined(PROGRAM_FILE) && abspath(PROGRAM_FILE) == abspath(@__FILE__)
     main()
 end
-=#
--- test/test_mysqlite.jl
+++ test/test_mysqlite.jl
@@ -1,17 +1,18 @@
 using Test
-
-include("../src/database_setup/sqlite.jl")
-using .MySQLite, DBInterface, SQLite, Arrow, Tables, DataFrames, CSV
+using DBInterface, SQLite, Arrow, Tables, DataFrames, CSV
+using hello_data_in_julia
+import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 
 sql = "SELECT * FROM t"
-schema1 = Tables.Schema((:id, :animal, :name), (Int32, String, String))
-schema = Tables.Schema((:USER_ID, :ACTION, :DATES), (Int32, String, Union{Missing,String}))
-data = [(1, "dog", "Margarita"), (2, "cat", "Michi"), (3, "bird", "Pantaleon")]
-file = CSV.File("data/csv/01/cancel.csv")
+schema = Tables.Schema((:id, :animal, :name), (Int32, String, String))
+data = CSV.Rows(IOBuffer("id,animal,name
+1,dog,Margarita
+2,cat,Michi
+3,bird,Pantaleon"); header=1, types=[Int32,String,String])
 
 
 @testset "MySQLite.get_conn" begin
-    MySQLite.get_conn() do conn
+    dbs.get_conn() do conn
         @test conn isa SQLite.DB
         stmt = SQLite.Stmt(conn, "SELECT 'HELLO, WORLD!' AS greet")
         result = DBInterface.execute(stmt)
@@ -22,11 +23,11 @@ end # testset
 
 
 @testset "MySQLite.sqlite_to_arrow" begin
-    MySQLite.get_conn() do conn
+    dbs.get_conn() do conn
         DBInterface.execute(conn, "CREATE TABLE t (id INTEGER, name TEXT)")
         DBInterface.execute(conn, "INSERT INTO t VALUES (1,'a'), (2,'b'), (3,'c'), (4,'d'), (5,'e')")
 
-        MySQLite.sqlite_to_arrow(conn, sql, "test_output")
+        dbs.sqlite_to_arrow(conn, sql, "test_output")
 
         @test isfile("data/arrow/test_output.arrow")
 
@@ -41,16 +42,27 @@ end # testset
 
 
 @testset "MySQLite.sqlite_sample" begin
-    MySQLite.get_conn() do conn
+    dbs.get_conn() do conn
         DBInterface.execute(conn, "CREATE TABLE t (id INTEGER, name TEXT)")
         DBInterface.execute(conn, "INSERT INTO t VALUES (1,'a'), (2,'b'), (3,'c'), (4,'d'), (5,'e')")
-        my_tables = MySQLite.my_tables(conn)
+        my_tables = dbs.my_tables(conn)
         @test "t" in my_tables
 
-        df = MySQLite.sqlite_sample(conn, sql)
+        df = dbs.sqlite_sample(conn, sql)
         @test df isa DataFrame
         @test nrow(df) == 5
         @test names(df) == ["id", "name"]
         @test df.name == ["a", "b", "c", "d", "e"]
     end
 end # testset
+
+
+@testset "MySQLite.csv_to_sqlite" begin
+    dbs.get_conn() do conn
+        SQLite.createtable!(conn, "family", schema, temp = false)
+        @test ("family" in dbs.my_tables(conn))
+        dbs.csv_to_sqlite(conn, "family", data)
+        df = dbs.sqlite_sample(conn, "SELECT * FROM family")
+        @test df.name = ["Margarita", "Michi", "Pantaleon"]
+    end
+end
