a5eaac6de6bc9ff5ecc68739d451f830a5b073d1
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-18 21:54:15 +0000

    Add data processing modules

--- src/database_setup/schemas.jl
+++ src/database_setup/schemas.jl
@@ -1,11 +1,11 @@
 module Schemas
 
-using TOML, OrderedCollections
+using TOML
 
-export SCHEMAS, my_data_types, load_schemas!
+export my_data_types, RAW_TOML
 
-const SCHEMAS = Dict{String, OrderedDict{Symbol, Type}}()
-
+schemas_path = joinpath(@__DIR__, "..", "..", "config", "schemas.toml")
+const SCHEMAS_TOML = Dict(table => content["columns"] for (table, content) in TOML.parsefile(schemas_path))
 
 """
     parse_type_string(type_str::String)
@@ -18,49 +18,14 @@ function parse_type_string(type_str::String)::Type
     end
 end
 
-#=
-"""
-    load_schemas!(toml_path::String)
-"""
-function load_schemas!(toml_path::String)
-    if !isfile(toml_path)
-        error("$toml_path: does not exist")
-    end
-
-    raw_data = TOML.parsefile(toml_path)
-    empty!(SCHEMAS)
-
-    for (table, columns) in raw_data
-        table_dict = OrderedDict{Symbol, Type}()
-        for (col_name, type_str) in columns
-            table_dict[Symbol(col_name)] = parse_type_string(type_str)
-        end
-        SCHEMAS[table] = table_dict
-    end
-
-    return SCHEMAS
-end
-
 
 """
     my_data_types(table::String)
 """
-function my_data_types(table::String)::Dict{Symbol, Type}
-    if isempty(SCHEMAS)
-        error("SCHEMAS dont loaded. Call 'Schemas.load_schemas!(path)' first.")
-    end
-
-    return get(SCHEMAS, table) do
-        error("'$table' isn't registered in the current TOML.")
+function my_data_types(table::String)::Vector{Pair{Symbol, Type}}
+    return map(SCHEMAS_TOML[table]) do col
+        Symbol(col["name"]) => parse_type_string(col["type"])
     end
 end
 
-
-function __init__()
-    default_path = joinpath(@__DIR__, "..", "..", "config", "schemas.toml")
-    if isfile(default_path)
-        load_schemas!(default_path)
-    end
-end =#
-
-end # module Schemas 
+end # module Schemas
