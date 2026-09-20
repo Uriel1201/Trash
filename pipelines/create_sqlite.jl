ef0713128dc0a372e61c70be2cc1b00a9edaf241
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-20 17:38:28 +0000

    Add data processing modules

--- pipelines/create_sqlite.jl
+++ pipelines/create_sqlite.jl
@@ -3,28 +3,24 @@ import hello_data_in_julia.DatabaseSetup.Schemas as sch
 import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 
 
-const TABLE_LIST = dbs.get_conn("hello_data", "ro") do conn
-    dbs.my_tables(conn)
-end
-
 function main(table::String)
-    println("Available Tables:")
-    for table in TABLE_LIST
-        println("  * $table")
-    end
+    dbs.get_conn("hello_data", "rw") do conn
+        TABLE_LIST = dbs.my_tables(conn)
+        println("Available Tables:")
+        for t in TABLE_LIST
+            println("  * $t")
+        end
+        if !(table in TABLE_LIST)
+            schema = sch.my_data_types(table)
+            columns = map(first, schema)
+            d_types = map(last, schema)
 
-    schema = sch.my_data_types(table)
-    columns = map(first, schema)
-    d_types = map(last, schema)
-    if !(table in TABLE_LIST)
-        dbs.get_conn("hello_data", "rw") do conn
             SQLite.createtable!(conn, table, Tables.Schema(columns, d_types), temp = false)
             @info "$table created"
             dbs.print_sqlite(conn, "PRAGMA table_info($table)")
-        end
-    else
-        @info "$table already exists"
-        dbs.get_conn("hello_data", "ro") do conn
+
+        else
+            @info "$table already exists"
             dbs.print_sqlite(conn, "PRAGMA table_info($table)")
         end
     end
