4f46921369135b9e4ea8a7efb4e5a70bab8f5fe8
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-20 02:00:55 +0000

    Add data processing modules

--- test/test_database_setup.jl
+++ test/test_database_setup.jl
@@ -11,10 +11,12 @@ schema = sch.my_data_types("family")
 
 @testset "loading TOML schemas" begin
     @test haskey(sch.SCHEMAS_TOML, "family")
-    @test schema isa Vector{Pair{Symbol, Type}}
+    @test schema isa Vector{Pair{Symbol,Type}}
     @test map(first, schema) == [:name, :gender, :birthday]
     @test map(last, schema) == [String, String, String]
-    @test_throws ErrorException("Table 'perro_del_mal' not found in schemas.toml") sch.my_data_types("perro_del_mal")
+    @test_throws ErrorException("Table 'perro_del_mal' not found in schemas.toml") sch.my_data_types(
+        "perro_del_mal",
+    )
 end # test_set
 
 
@@ -31,8 +33,14 @@ end # testset
 
 @testset "converting SQLite query to IPC file" begin
     dbs.get_conn() do conn
-        DBInterface.execute(conn, "CREATE TABLE family (name TEXT, genre TEXT, birthday TEXT)")
-        DBInterface.execute(conn, "INSERT INTO family VALUES ('Margarita', 'dog', '11-Jan-2018'), ('Uriel', 'human', '01-Dic-93'), ('Angel', 'human', '06-06-2007')")
+        DBInterface.execute(
+            conn,
+            "CREATE TABLE family (name TEXT, genre TEXT, birthday TEXT)",
+        )
+        DBInterface.execute(
+            conn,
+            "INSERT INTO family VALUES ('Margarita', 'dog', '11-Jan-2018'), ('Uriel', 'human', '01-Dic-93'), ('Angel', 'human', '06-06-2007')",
+        )
 
         dbs.sqlite_to_arrow(conn, sql, "test_output")
 
@@ -44,14 +52,17 @@ end # testset
         @test collect(tbl.genre) == ["dog", "human", "human"]
     end
 
-    rm("data/arrow/test_output.arrow"; force=true)
+    rm("data/arrow/test_output.arrow"; force = true)
 end # testset
 
 
 @testset "reading query results into a DataFrame" begin
     dbs.get_conn() do conn
         DBInterface.execute(conn, "CREATE TABLE t (id INTEGER, name TEXT)")
-        DBInterface.execute(conn, "INSERT INTO t VALUES (1,'a'), (2,'b'), (3,'c'), (4,'d'), (5,'e')")
+        DBInterface.execute(
+            conn,
+            "INSERT INTO t VALUES (1,'a'), (2,'b'), (3,'c'), (4,'d'), (5,'e')",
+        )
         my_tables = dbs.my_tables(conn)
         @test "t" in my_tables
 
@@ -66,15 +77,26 @@ end # testset
 
 columns = map(first, schema)
 data_types = map(last, schema)
-data = CSV.Rows(IOBuffer("alias,animal,cumpleaños
+data = CSV.Rows(
+    IOBuffer("alias,animal,cumpleaños
 Margarita,dog,11-Jan-2018
 Uriel,human,12-Dic-1993
-Angel,human,06-Jan-2007"); header=columns, types=data_types, skipto=2)
+Angel,human,06-Jan-2007");
+    header = columns,
+    types = data_types,
+    skipto = 2,
+)
 @testset "loading a csv file to SQLite" begin
     dbs.get_conn() do conn
-        SQLite.createtable!(conn, "family", Tables.Schema(columns, data_types), temp = false)
+        SQLite.createtable!(
+            conn,
+            "family",
+            Tables.Schema(columns, data_types),
+            temp = false,
+        )
         @test ("family" in dbs.my_tables(conn))
-        @test dbs.insert_query("family", columns) == "INSERT INTO family (name, gender, birthday) VALUES (?, ?, ?)"
+        @test dbs.insert_query("family", columns) ==
+              "INSERT INTO family (name, gender, birthday) VALUES (?, ?, ?)"
         dbs.csv_to_sqlite(conn, "family", data)
         df = dbs.sqlite_sample(conn, "SELECT * FROM family")
         @test df.name == ["Margarita", "Uriel", "Angel"]
