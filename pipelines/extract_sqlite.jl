b3b11cfd255c719a930061b854fe6a9908c31bd1
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-21 04:35:48 +0000

    Add data processing modules

---
+++ pipelines/extract_sqlite.jl
@@ -0,0 +1,22 @@
+module ESQLite
+
+import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
+
+function main(table::String, query_file::String)
+    file = joinpath(@__DIR__, "..", "oltp", query_file)
+    dbs.get_conn("hello_data", "ro") do conn
+        if isfile(file) && table in dbs.my_tables(conn)
+            query = replace(read(file, String), "{table}" => table)
+            dbs.sqlite_to_arrow(conn, query, table)
+        else
+            throw(ArgumentError("File $file or table $table not found"))
+        end
+    end
+end
+
+
+if Base.@isdefined(PROGRAM_FILE) && abspath(PROGRAM_FILE) == abspath(@__FILE__)
+    main(ARGS[1], ARGS[2])
+end
+
+end # module ESQLite
