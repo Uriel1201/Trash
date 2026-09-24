87bec68d4a766d7b92fc4b873b025447383d7c32
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-23 23:42:37 -0600

    Rename extract_table function to extract_sqlite

--- pipelines/extract_sqlite.jl
+++ pipelines/extract_sqlite.jl
@@ -1,6 +1,6 @@
 import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 
-function extract_table(table::String)
+function extract_sqlite(table::String)
     dbs.get_conn("hello_data", "ro") do conn
         dbs.create_arrow(conn, table)
     end
@@ -8,5 +8,5 @@ end # extract_table
 
 
 if Base.@isdefined(PROGRAM_FILE) && abspath(PROGRAM_FILE) == abspath(@__FILE__)
-    extract_table(ARGS[1])
+    extract_sqlite(ARGS[1])
 end
