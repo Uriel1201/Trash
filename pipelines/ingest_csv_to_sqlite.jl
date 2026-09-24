aeccddafde185b8c450e2baaf06ce322b302dba2
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-24 04:10:52 +0000

    Add data processing modules

--- pipelines/ingest_csv_to_sqlite.jl
+++ pipelines/ingest_csv_to_sqlite.jl
@@ -1,11 +1,9 @@
-module IngestSQLite
-
 using CSV
 import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 import hello_data_in_julia.DatabaseSetup.Schemas as sch
 
 
-function main(table::String, csv_file::String)
+function csv_to_sqlite(table::String, csv_file::String)
     csv_path = joinpath(@__DIR__, "..", "data", "csv", csv_file)
     if isfile(csv_path)
         schema = sch.my_data_types(table)
@@ -16,17 +14,17 @@ function main(table::String, csv_file::String)
             skipto = 2,
         )
         dbs.get_conn("hello_data", "rw") do conn
-            dbs.csv_to_sqlite(conn, table, data)
-            @info "$csv_path ingested"
+            dbs.ingest_csv(conn, table, data)
+            println("***csv file $csv_path ingested***")
         end
     else
-        throw(ArgumentError("$csv_path not found in directory"))
+        throw(ArgumentError("File $csv_path not found in directory"))
     end
-end
-
-end # module IngestSQLite
+end # csv_to_sqlite
 
 
 if Base.@isdefined(PROGRAM_FILE) && abspath(PROGRAM_FILE) == abspath(@__FILE__)
-    main(ARGS[1], ARGS[2])
+    csv_to_sqlite(ARGS[1], ARGS[2])
 end
+
+
