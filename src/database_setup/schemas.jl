c971b98e30997daea796c001bc06534cc4f87400
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-24 17:37:41 -0600

    Update schemas.jl

--- src/database_setup/schemas.jl
+++ src/database_setup/schemas.jl
@@ -23,9 +23,6 @@ end
     my_data_types(table::String)
 """
 function my_data_types(table::String)::Vector{Pair{Symbol, Type}}
-    if !haskey(SCHEMAS_TOML, table)
-        error("Table '$table' not found in schemas.toml")
-    end
     return map(SCHEMAS_TOML[table]) do col
         Symbol(col["name"]) => parse_type_string(col["type"])
     end
