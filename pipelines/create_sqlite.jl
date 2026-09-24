f4a93f8564a6863241fa384452f640a411bbed0b
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-24 05:18:51 +0000

    Add data processing modules

--- pipelines/create_sqlite.jl
+++ pipelines/create_sqlite.jl
@@ -1,11 +1,9 @@
-module CreateSQLite
-
 using Tables, SQLite
 import hello_data_in_julia.DatabaseSetup.Schemas as sch
 import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 
 
-function main(table::String)
+function create_table(table::String)
     dbs.get_conn("hello_data", "rw") do conn
         TABLE_LIST = dbs.my_tables(conn)
         println("Available Tables:")
@@ -16,18 +14,18 @@ function main(table::String)
             schema = sch.my_data_types(table)
             columns = map(first, schema)
             d_types = map(last, schema)
-    
+
             SQLite.createtable!(conn, table, Tables.Schema(columns, d_types), temp = false)
-            println("\n***$table created***")
+            println("
+***$table created***")
             dbs.print_sqlite(conn, "PRAGMA table_info($table)")
         else
             throw(KeyError(table))
         end
     end
-end
-end #module CreateSQLite
+end # create_table
 
 
 if Base.@isdefined(PROGRAM_FILE) && abspath(PROGRAM_FILE) == abspath(@__FILE__)
-    main(ARGS[1])
+    create_table(ARGS[1])
 end
