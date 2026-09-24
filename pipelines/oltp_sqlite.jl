e0db8e47c97470702f07e2b22e4b5e491e7f3307
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-24 05:39:15 +0000

    Add data processing modules

--- pipelines/oltp_sqlite.jl
+++ pipelines/oltp_sqlite.jl
@@ -1,10 +1,8 @@
-module QueryingSQLite
-
 import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 import hello_data_in_julia.DatabaseSetup.Schemas as sch
 
 
-function main(table::String, query_file::String)
+function oltp_sqlite(table::String, query_file::String)
     file = joinpath(@__DIR__, "..", "oltp", query_file)
     dbs.get_conn("hello_data", "ro") do conn
         if isfile(file) && table in dbs.my_tables(conn)
@@ -16,9 +14,7 @@ function main(table::String, query_file::String)
     end
 end
 
-end # module QueryingSQLite
-
 
 if Base.@isdefined(PROGRAM_FILE) && abspath(PROGRAM_FILE) == abspath(@__FILE__)
-    main(ARGS[1], ARGS[2])
+    oltp_sqlite(ARGS[1], ARGS[2])
 end
