56cd743486667e63cde12d83c564804f7265089a
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-24 05:27:23 +0000

    Add data processing modules

--- pipelines/extract_sqlite.jl
+++ pipelines/extract_sqlite.jl
@@ -1,18 +1,12 @@
-module ExtractSQLite
-
 import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 
-function main(table::String)
+function extract_table(table::String)
     dbs.get_conn("hello_data", "ro") do conn
         dbs.create_arrow(conn, table)
     end
-end
+end # extract_table
 
 
 if Base.@isdefined(PROGRAM_FILE) && abspath(PROGRAM_FILE) == abspath(@__FILE__)
-    main(ARGS[1])
+    extract_table(ARGS[1])
 end
-
-end # module ExtractSQLite
-
-
