c223c7cd30aa9d92a2ce04dfe4e804393c3bebe3
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-24 03:46:11 +0000

    Add data processing modules

--- pipelines/extract_sqlite.jl
+++ pipelines/extract_sqlite.jl
@@ -2,21 +2,17 @@ module ExtractSQLite
 
 import hello_data_in_julia.DatabaseSetup.MySQLite as dbs
 
-function main(table::String, query_file::String)
-    file = joinpath(@__DIR__, "..", "oltp", query_file)
+function main(table::String)
     dbs.get_conn("hello_data", "ro") do conn
-        if isfile(file) && table in dbs.my_tables(conn)
-            query = replace(read(file, String), "{table}" => table)
-            dbs.sqlite_to_arrow(conn, query, table)
-        else
-            throw(ArgumentError("File $file or table $table not found"))
-        end
+        dbs.create_arrow(conn, table)
     end
 end
 
 
 if Base.@isdefined(PROGRAM_FILE) && abspath(PROGRAM_FILE) == abspath(@__FILE__)
-    main(ARGS[1], ARGS[2])
+    main(ARGS[1])
 end
 
 end # module ExtractSQLite
+
+
